c020fdfd87ac82ee68c1869c294e395959b56f88
Ananthan4451 <ananthakrishna97gmail.com>
2019-12-31 15:07:15 +0530

    saved

--- student.py
+++ student.py
@@ -2,6 +2,7 @@ from tkinter import *
 import sqlite3
 from tkinter import messagebox
 
+
 class DB:
     def __init__(self):
         try:
@@ -16,7 +17,6 @@ class DB:
 
     def __del__(self):
         self.conn.close()
-
 
     def view(self):
         self.cur.execute("SELECT * FROM student")
@@ -56,7 +56,7 @@ def get_selected_row(event):
         txtaddress.delete(0, END)
         txtaddress.insert(END, selected_tuple[4])
     except:
-        messagebox.showinfo("Error","please select a value")
+        messagebox.showinfo("Error", "please select a value")
 
 
 def view_command():
@@ -70,7 +70,8 @@ def view_command():
 
 
 def add_command():
-    if name_text.get() and branch_text.get() and number_text.get().isnumeric() == True and len(number_text.get()) == 10 and address_text.get():
+    if name_text.get() and branch_text.get() and number_text.get().isnumeric() == True and len(
+            number_text.get()) == 10 and address_text.get():
         db.insert(name_text.get(), branch_text.get(), number_text.get(), address_text.get())
         list1.delete(0, END)
         list1.insert(END, (name_text.get(), branch_text.get(), number_text.get(), address_text.get()))
@@ -82,7 +83,6 @@ def add_command():
         txtaddress.delete(0, END)
     else:
         messagebox.showinfo("Error", "Fill all fields with correct values")
-
 
 
 def delete_command():
@@ -133,20 +133,25 @@ def on_closing():
     if messagebox.askokcancel("Quit", "Do you want to quit?"):
         root.destroy()
         del dd
+
+
 root.protocol("WM_DELETE_WINDOW", on_closing)  # handle window closing
 
-# heading
+#-------------------------- heading------------------------------------------
+
 lblinfo = Label(root, font=('aria', 30, 'bold'), text="STUDENT MANAGEMENT", fg="black", bd=10)
 lblinfo.place(x=330, y=30)
 
-lblname = Label(root, font=('aria', 16, 'bold'), text="Name",  bd=10)
+#-------------------enrty & label--------------------------------
+
+lblname = Label(root, font=('aria', 16, 'bold'), text="Name", bd=10)
 lblname.place(x=40, y=130)
 name_text = StringVar()
 txtname = Entry(root, font=('ariel', 16, 'bold'), bd=6, insertwidth=4,
                 textvariable=name_text)
 txtname.place(x=250, y=130)
 
-lblbranch = Label(root, font=('aria', 16, 'bold'), text="Branch",  bd=10)
+lblbranch = Label(root, font=('aria', 16, 'bold'), text="Branch", bd=10)
 lblbranch.place(x=40, y=180)
 branch_text = StringVar()
 txtbranch = Entry(root, font=('ariel', 16, 'bold'), bd=6, insertwidth=4,
@@ -160,16 +165,17 @@ txtnumber = Entry(root, font=('ariel', 16, 'bold'), bd=6, insertwidth=4,
                   textvariable=number_text)
 txtnumber.place(x=250, y=230)
 
-lbladdress = Label(root, font=('aria', 16, 'bold'), text="Place",  bd=10, anchor='w')
+lbladdress = Label(root, font=('aria', 16, 'bold'), text="Place", bd=10, anchor='w')
 lbladdress.place(x=40, y=280)
 address_text = StringVar()
 txtaddress = Entry(root, font=('ariel', 16, 'bold'), bd=6, insertwidth=4,
                    textvariable=address_text)
 txtaddress.place(x=250, y=280)
-#button
+
+#---------------------------------------- button---------------------------
 
 btninsert = Button(root, bd=8, fg="black", font=('ariel', 16, 'bold'), width=8, text="Insert",
-                  command=add_command)
+                   command=add_command)
 btninsert.place(x=70, y=350)
 
 btnview = Button(root, bd=8, fg="black", font=('ariel', 16, 'bold'), width=8, text="View All",
@@ -177,18 +183,19 @@ btnview = Button(root, bd=8, fg="black", font=('ariel', 16, 'bold'), width=8, te
 btnview.place(x=210, y=350)
 
 btndelete = Button(root, bd=8, fg="black", font=('ariel', 16, 'bold'), width=8, text="Delete",
-                 command=delete_command)
+                   command=delete_command)
 btndelete.place(x=350, y=350)
 
 btnupdate = Button(root, bd=8, fg="black", font=('ariel', 16, 'bold'), width=8, text="Update",
-                    command=update_command)
+                   command=update_command)
 btnupdate.place(x=150, y=420)
 
 btnclear = Button(root, bd=8, fg="black", font=('ariel', 16, 'bold'), width=8, text="Clear",
-                 command=clear_command)
+                  command=clear_command)
 btnclear.place(x=300, y=420)
 
-# list
+#----------------------------------list----------------------------
+
 list1 = Listbox(root, height=20, width=85)
 list1.place(x=550, y=130)
 list1.bind('<<ListboxSelect>>', get_selected_row)
